ed27f958565816ee9380c014527442652361d1ba
samurai <150229185+RS333-creater@users.noreply.github.com>
2024-11-12 12:33:22 +0900

    Initial commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$kernel->bootstrap();
+
+use App\Models\User;
+
+$users = User::all();
+
+dd($users);
